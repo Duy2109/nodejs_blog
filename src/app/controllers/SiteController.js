ee0ce2102d66738634b1ab57b9f80eb8fcf6dd59
Duy2109 <106510256+Duy2109@users.noreply.github.com>
2023-10-29 23:41:00 +0700

    CRUD

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,25 @@
+
+const Course = require('../models/Course');
+const {mutipleMongooseToObject} = require('../../until/mongoose')
+
 class SiteController {
-    //[GET] /news
-    index(req, res) {
-        res.render('home');
-    }
+  //[GET] /news
+   index(req, res, next) {
+     Course.find({})
+        .then(courses => 
+            res.render('home',{
+              courses:mutipleMongooseToObject(courses)
+            }))
+        .catch(next);
 
+  }
 
-    //[GET] /search
-    search(req, res) {
-        res.render('search');
-    }
+  //[GET] /search
+
+  
+  search(req, res) {
+    res.render("search");
+  }
 }
 
-module.exports = new SiteController
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
-const handlebars = require('express-handlebars');
+const methodOverride=require("method-override");
+const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
-const route = require('./routes');
+const route = require("./routes");
+const db = require('./config/db');
 
+db.connect();
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+  );
+app.use(express.json());
+app.use(methodOverride('_method'))
 route(app);
-
-app.use(express.urlencoded({
-  extended:true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 //HTTP logger
 // app.use(morgan("combined"));
 
 //template engine
-app.engine("hbs", handlebars.engine({
-  extname:".hbs"
-}));
-app.set('view engine','hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
-
-
-
-
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  }),
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources","views"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
+ 
